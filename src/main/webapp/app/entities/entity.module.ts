1cbc09568d18ac9650ef639bba8b171f1eb2028c
Martin <martinetex18@gmail.com>
2018-02-04 01:47:26 +0100

    Proyecto con angular 5 y JDL listo

--- src/main/webapp/app/entities/entity.module.ts
+++ src/main/webapp/app/entities/entity.module.ts
@@ -1,9 +1,35 @@
 import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 
+import { RocktionaryBandaModule } from './banda/banda.module';
+import { RocktionaryDiscograficaModule } from './discografica/discografica.module';
+import { RocktionaryComponenteModule } from './componente/componente.module';
+import { RocktionaryPuntuacionBandaModule } from './puntuacion-banda/puntuacion-banda.module';
+import { RocktionaryPuntuacionAlbumModule } from './puntuacion-album/puntuacion-album.module';
+import { RocktionaryPuntuacionCancionModule } from './puntuacion-cancion/puntuacion-cancion.module';
+import { RocktionaryAlbumModule } from './album/album.module';
+import { RocktionaryCancionModule } from './cancion/cancion.module';
+import { RocktionaryUserExtModule } from './user-ext/user-ext.module';
+import { RocktionaryComentarCancionModule } from './comentar-cancion/comentar-cancion.module';
+import { RocktionaryComentarAlbumModule } from './comentar-album/comentar-album.module';
+import { RocktionaryComentarBandaModule } from './comentar-banda/comentar-banda.module';
+import { RocktionaryUserFollowingUserModule } from './user-following-user/user-following-user.module';
 /* jhipster-needle-add-entity-module-import - JHipster will add entity modules imports here */
 
 @NgModule({
     imports: [
+        RocktionaryBandaModule,
+        RocktionaryDiscograficaModule,
+        RocktionaryComponenteModule,
+        RocktionaryPuntuacionBandaModule,
+        RocktionaryPuntuacionAlbumModule,
+        RocktionaryPuntuacionCancionModule,
+        RocktionaryAlbumModule,
+        RocktionaryCancionModule,
+        RocktionaryUserExtModule,
+        RocktionaryComentarCancionModule,
+        RocktionaryComentarAlbumModule,
+        RocktionaryComentarBandaModule,
+        RocktionaryUserFollowingUserModule,
         /* jhipster-needle-add-entity-module - JHipster will add entity modules here */
     ],
     declarations: [],
