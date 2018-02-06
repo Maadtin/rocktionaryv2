4bf3d2280131becedfb0cbdd5a1d6e99007fe676
Martin <martinetex18@gmail.com>
2018-02-06 13:02:34 +0100

    Entities readyyy

---
+++ src/main/java/AngularRouteFilter.java
@@ -0,0 +1,38 @@
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import javax.servlet.FilterChain;
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.regex.Pattern;
+
+public class AngularRouteFilter extends OncePerRequestFilter {
+
+    // add the values you want to redirect for
+    private static final Pattern PATTERN = Pattern.compile("^/((api|swagger-ui|management|swagger-resources)/|favicon\\.ico|v2/api-docs).*");
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request,
+                                    HttpServletResponse response,
+                                    FilterChain filterChain)
+        throws ServletException, IOException {
+        if (isServerRoute(request)) {
+            filterChain.doFilter(request, response);
+        } else {
+            RequestDispatcher rd = request.getRequestDispatcher("/");
+            rd.forward(request, response);
+        }
+    }
+
+    protected static boolean isServerRoute(HttpServletRequest request) {
+        if (request.getMethod().equals("GET")) {
+            String uri = request.getRequestURI();
+            return PATTERN.matcher(uri).matches();
+        }
+        return true;
+    }
+
+
+}
--- src/main/webapp/app/layouts/navbar/navbar.route.ts
+++ src/main/webapp/app/layouts/navbar/navbar.route.ts
@@ -2,7 +2,7 @@ import { Route } from '@angular/router';
 
 import { NavbarComponent } from './navbar.component';
 
-export const navbarRoute: Route = {
+export const navbarRoute : Route = {
     path: '',
     component: NavbarComponent,
     outlet: 'navbar'
