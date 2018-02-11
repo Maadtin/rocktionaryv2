0997a2a4a6db2e78b4175ebf92f72807b318433a
Martin <martinetex18@gmail.com>
2018-02-11 17:38:34 +0100

    Búsqueda de home ya devulve resultados y se conectar con la API de spotify correctamente.

--- src/main/java/com/rocktionary/web/rest/AccountResource.java
+++ src/main/java/com/rocktionary/web/rest/AccountResource.java
@@ -2,24 +2,34 @@ package com.rocktionary.web.rest;
 
 import com.codahale.metrics.annotation.Timed;
 
+import com.google.gson.Gson;
 import com.rocktionary.domain.User;
 import com.rocktionary.repository.UserRepository;
 import com.rocktionary.security.SecurityUtils;
 import com.rocktionary.service.MailService;
 import com.rocktionary.service.UserService;
+import com.rocktionary.service.dto.SpotifyToken;
 import com.rocktionary.service.dto.UserDTO;
 import com.rocktionary.web.rest.errors.*;
 import com.rocktionary.web.rest.vm.KeyAndPasswordVM;
 import com.rocktionary.web.rest.vm.ManagedUserVM;
 
+import okhttp3.FormBody;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
 import org.apache.commons.lang3.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+import org.springframework.web.servlet.view.RedirectView;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.validation.Valid;
+import java.io.IOException;
 import java.util.*;
 
 /**
@@ -185,5 +195,71 @@ public class AccountResource {
         return !StringUtils.isEmpty(password) &&
             password.length() >= ManagedUserVM.PASSWORD_MIN_LENGTH &&
             password.length() <= ManagedUserVM.PASSWORD_MAX_LENGTH;
+    }
+
+    //login spotify
+    private String generateRandomString (int length) {
+        StringBuilder text = new StringBuilder();
+        String possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+
+        for (int i = 0; i < length; i++) {
+            text.append(possible.charAt((int) Math.floor(Math.random() * possible.length())));
+        }
+        return text.toString();
+    }
+
+    // spotify auth
+
+
+    @GetMapping("/account/login-spotify")
+    public RedirectView testSpotify (RedirectAttributes attrs) {
+        String state = generateRandomString(16);
+        attrs.addAttribute("response_type", "code");
+        attrs.addAttribute("client_id", "acb078a8d60f4603bfbfb488651a6ca4");
+        attrs.addAttribute("scope", "user-read-private user-read-email user-follow-read");
+        attrs.addAttribute("redirect_uri", "http://localhost:8080/api/account/callback");
+        attrs.addAttribute("state", state);
+        attrs.addAttribute("show_dialog", "true");
+        return new RedirectView("https://accounts.spotify.com/authorize");
+
+    }
+
+    @GetMapping("/account/callback")
+    public ModelAndView handleOnSpotifyCallBackUrl (@RequestParam("code") String code) throws IOException {
+
+        okhttp3.RequestBody body = new FormBody.Builder()
+            .add("grant_type", "authorization_code")
+            .add("code", code)
+            .add("redirect_uri", "http://localhost:8080/api/account/callback")
+            .build();
+
+
+        Request request = new Request.Builder()
+            .url("https://accounts.spotify.com/api/token")
+            .addHeader("Authorization",
+                "Basic " + new String(
+                    Base64
+                        .getEncoder()
+                        .encode("acb078a8d60f4603bfbfb488651a6ca4:de997066a3f84698841351de1faa999a"
+                            .getBytes())))
+            .post(body)
+            .build();
+
+        OkHttpClient client = new OkHttpClient.Builder().build();
+        Response response = client.newCall(request).execute();
+
+        String res = "";
+        if (response.isSuccessful()) {
+            res = response.body().string();
+        }
+
+        Gson gson = new Gson();
+        SpotifyToken spotifyToken = gson.fromJson(res, SpotifyToken.class);
+
+        Map<String, String> map = new HashMap<>();
+        map.put("token", spotifyToken.getAccessToken());
+        map.put("refresh_token", spotifyToken.getRefreshToken());
+
+        return new ModelAndView(new RedirectView("http://localhost:8080"), map);
     }
 }
--- src/main/webapp/app/home/home.component.ts
+++ src/main/webapp/app/home/home.component.ts
@@ -4,6 +4,8 @@ import { JhiEventManager } from 'ng-jhipster';
 
 import { Account, LoginModalService, Principal } from '../shared';
 
+import { HomeService } from './home.service'
+
 @Component({
     selector: 'jhi-home',
     templateUrl: './home.component.html',
@@ -16,14 +18,23 @@ export class HomeComponent implements OnInit {
     account: Account;
     modalRef: NgbModalRef;
 
-    constructor(
-        private principal: Principal,
-        private loginModalService: LoginModalService,
-        private eventManager: JhiEventManager
-    ) {
+    private inputSearchText: string;
+    private placeHolderText: string;
+    private clickedButton: string;
+    private searchCriteria: string;
+    private timeOut: any;
+
+    constructor(private principal: Principal,
+                private loginModalService: LoginModalService,
+                private eventManager: JhiEventManager,
+                private homeService: HomeService) {
     }
 
     ngOnInit() {
+        this.placeHolderText = 'albumes';
+        this.clickedButton = 'albumes';
+        this.searchCriteria = 'album';
+
         this.principal.identity().then((account) => {
             this.account = account;
         });
@@ -45,4 +56,56 @@ export class HomeComponent implements OnInit {
     login() {
         this.modalRef = this.loginModalService.open();
     }
+
+    // métodos de búsqueda
+    handleOnChangePlaceHolderText(filter: string): void {
+        this.placeHolderText = filter;
+        this.clickedButton = filter;
+        this.handleOnSearchCriteriaChange(this.placeHolderText);
+        console.log(this.searchCriteria)
+    }
+
+    handleOnSearchCriteriaChange (searchCriteria: string) {
+        switch (searchCriteria) {
+            case 'albumes':
+                this.searchCriteria = 'album';
+                break;
+            case 'canciones':
+                this.searchCriteria = 'track';
+                break;
+            case 'bandas':
+                this.searchCriteria = 'artist';
+                break;
+        }
+    }
+
+    handleOnInputSearch() {
+        const params: any = {
+            searchCriteria: this.searchCriteria,
+            searchQuery: this.inputSearchText
+        };
+        clearTimeout(this.timeOut);
+        this.timeOut = setTimeout(() => {
+            this.homeService
+                .getSearchResults(params)
+                .subscribe(
+                    this.handleOnSuccess,
+                    this.handleOnError
+                )
+        }, 500);
+    }
+
+    // error handling
+    handleOnSuccess (res) { console.log(res)}
+    handleOnError (err) {
+        switch (err.status) {
+            case 401:break;
+            case 404:break;
+            case 400:break;
+        }
+    }
+
 }
+
+
+
